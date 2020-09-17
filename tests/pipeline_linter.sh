23fab26075cddc29c52191f54f5b2a701a351cc2
Serhii Turivnyi <turivniy@gmail.com>
2020-09-17 13:21:23 +0200

    Remove token, add description (#13)
    * Remove token, add description

--- tests/pipeline_linter.sh
+++ tests/pipeline_linter.sh
@@ -4,12 +4,11 @@ while getopts t:f: option
   do
     case "${option}"
       in
-        t) TOKEN=${OPTARG};;
         f) FILE=${OPTARG};;
     esac
   done
 
-OUTPUT=$(curl -H "Authorization: OAuth $TOKEN" -X POST -F "jenkinsfile=<$FILE" https://osci-jenkins-2.ci.fedoraproject.org/pipeline-model-converter/validate)
+OUTPUT=$(curl -H "Authorization: OAuth " -X POST -F "jenkinsfile=<$FILE" https://osci-jenkins-2.ci.fedoraproject.org/pipeline-model-converter/validate)
 
 if [[ "$OUTPUT" =~ .*"successfully validated".* ]]; then
   exit 0
